02750477c1cc2b47603f82c4ea2e874baa07426f
Erza <70858831+riadkabir45@users.noreply.github.com>
2023-05-19 15:36:57 +0600

    feat: get time table

--- user-basic-info.py
+++ user-basic-info.py
@@ -2,6 +2,7 @@ import urllib.request
 import PyPDF2
 import re
 import csv
+import tabula
 import os.path as fileCheck
 from os import remove as fileRem
 
@@ -16,6 +17,8 @@ def getData(jsesID,servID='1684732',timeID='627120',file='data.pdf'):
         urllib.request.install_opener(opener)
         urllib.request.urlretrieve(url, file)
         return True
+    except KeyboardInterrupt:
+        exit()
     except:
         return False
 
@@ -30,32 +33,57 @@ def parseData(file='data.pdf'):
         id = re.search("ID : [0-9]+", text).group()[5:]
         pdf_file.close()
         return [id,pg,nm]
+    except KeyboardInterrupt:
+        exit()
     except:
         return False
 
-def storeData(sid,id,pg,name,dataname='data.csv'):
+def parseTime(file='data.pdf'):
+    try:
+        times = []
+        datasheet = tabula.read_pdf(file,pages=1)[0]
+        for index in datasheet:
+            if index == 'Time/Day':
+                continue
+            for i in range(len(datasheet[index])):
+                data = str(datasheet[index][i])
+                if data == 'nan':
+                    continue
+                data = data.replace('\r',' ')
+                time = str(datasheet['Time/Day'][i]).replace('\r','')
+                times.append(index[:3]+"("+time+" - "+data+")")
+        return times
+    except KeyboardInterrupt:
+        exit()
+    except:
+        return False
+
+def storeData(sid,id,pg,name,times,dataname='data.csv'):
+    if len(times) == 0:
+        tb = "NULL"
+    else:
+        tb = ','.join(times).strip('"')
     with open(dataname, 'a') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow([sid,id,pg,name])
+        csvwriter.writerow([sid,id,pg,name,tb])
 
-start=1669207
-steps=30000
-sessionID='97CC31CF14E6D3AB3640A0E3CEF73929'
+def getAllData(sessionID,start=1669207,steps=1,timeID=627120):
+    for i in range(start,start+steps):
+        if fileCheck.exists('killme'):
+            fileRem('killme')
+            break
+        if fileCheck.exists('data.pdf'):
+            fileRem('data.pdf')
+        print("---------------")
+        print("Getting Data",i,i-start)
+        try:
+            getData(sessionID,i,timeID=timeID)
+            data = parseData()
+            timeTable = parseTime()
+            id,pg,nm = data
+            storeData(i,id,pg,nm,timeTable)
+            print("Data Parsed")
+        except:
+            print("Failed Parsing")
 
-
-for i in range(start,start+steps):
-    if fileCheck.exists('killme'):
-        fileRem('killme')
-        break
-    print("---------------")
-    print("Getting Data",i,i-start)
-    try:
-        getData(sessionID,i)
-        data = parseData()
-        id,pg,nm = parseData()
-        storeData(i,id,pg,nm)
-        print("Data Parsed")
-    except:
-        print("Failed Parsing")
-    if fileCheck.exists('data.pdf'):
-        fileRem('data.pdf')
+getAllData("E8AFD1DC58F907157E1CFBB52CFB51A0",steps=100000,start=1684732,timeID=627121)
